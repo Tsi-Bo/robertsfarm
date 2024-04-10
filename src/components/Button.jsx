997b53db765dc75735477ef22d0a29417bb2eeb3
TSIBO <81839976+Tsi-Bo@users.noreply.github.com>
2024-04-10 14:57:35 -0400

    Add files via upload

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,24 +1,35 @@
 import styled from "styled-components";
 
 const StyledButton = styled.button`
-  /* margin-top: var(--gap-l); */
-  padding: 10px 20px;
-  font-size: var(--fs-m);
-  border: 2px solid black;
-  border-radius: 5px;
-  background-color: transparent; /* Change to your desired button color */
-  color: black;
+  background-color: ${(props) =>
+    props.variant === "contained" ? `var(--yellow)` : "white"};
+  border: ${(props) =>
+    props.variant === "outlined" ? `1px solid ${props.color}` : "none"};
+  color: ${(props) => (props.variant === "outlined" ? props.color : "white")};
+  padding: var(--pad-l) var(--pad-xl);
   cursor: pointer;
-  transition: all 0.3s ease; /* Smooth transition for button hover */
+  border-radius: 5px;
+  font-size: var(--fs-m);
+  outline: none;
+  opacity: ${(props) => (props.disabled ? 0.5 : 1)};
 
   &:hover {
-    letter-spacing: 0.4rem;
+    opacity: ${(props) => (props.disabled ? 0.5 : 0.8)};
   }
 `;
 
+Button.defaultProps = {
+  variant: "contained",
+  disabled: false,
+};
+
 // eslint-disable-next-line react/prop-types
-function Button({ children }) {
-  return <StyledButton>{children}</StyledButton>;
+function Button({ onClick, children, color, variant, disabled }) {
+  return (
+    <StyledButton onClick={onClick} variant={variant} disabled={disabled}>
+      {children}
+    </StyledButton>
+  );
 }
 
 export default Button;
